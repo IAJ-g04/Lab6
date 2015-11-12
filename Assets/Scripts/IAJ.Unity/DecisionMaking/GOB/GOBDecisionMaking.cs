f72ed9969d20786b81516e9182ebb2f81f57d918
Kaguedes <tiagoasc.69@hotmail.com>
2015-11-12 17:49:56 +0000

    chooseaction

--- Assets/Scripts/IAJ.Unity/DecisionMaking/GOB/GOBDecisionMaking.cs
+++ Assets/Scripts/IAJ.Unity/DecisionMaking/GOB/GOBDecisionMaking.cs
@@ -13,8 +13,24 @@ namespace Assets.Scripts.IAJ.Unity.DecisionMaking.GOB
 
         public static Action ChooseAction(List<Action> actions, List<Goal> goals)
         {
-            //TODO implement
-            throw new NotImplementedException();
+            float iv = 0.0f;
+            Goal bestg = null;
+            Action act = null;
+            foreach(Goal g in goals)
+            {
+                if(g.InsistenceValue > iv)
+                {
+                    iv = g.InsistenceValue;
+                    bestg = g;
+                }
+            }
+            foreach(Action a in actions)
+            {
+                if (a.Name.Equals(bestg.Name)){
+                    act = a;
+                }
+            }
+            return act;
         }
     }
 }
